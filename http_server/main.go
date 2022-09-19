7f422b303c20351f7d553a0b5add0742b77f2700
Jack Lin <jackzlin@deloitte.com.cn>
2022-09-19 22:36:20 +0800

    added http middleward

---
+++ http_server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main(){
+
+ f := http.HandlerFunc( hello)
+ l := logger{Inner: f}
+
+ http.ListenAndServe(":8000", &l)
+
+}
+
+type logger struct {
+	Inner http.Handler
+}
+
+func (l *logger) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	log.Println("start")
+	l.Inner.ServeHTTP(w, r)
+	log.Println("finish")
+
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello\n")
+}
